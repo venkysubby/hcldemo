1a3da50e6531df94a4dbaaec33930bc123f64590
venkysubby <91669076+venkysubby@users.noreply.github.com>
2022-11-10 12:23:48 +0000

    Update main.tf

--- shuuu/main.tf
+++ shuuu/main.tf
@@ -30,10 +30,3 @@ resource "azurerm_kubernetes_cluster" "aks" {
     Environment = "Production"
   }
 }
-
-resource "azurerm_role_assignment" "acrpull" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-}
