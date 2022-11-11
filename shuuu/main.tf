a133fd3662fd21be954f0e030abe9b51783b3a84
venkysubby <91669076+venkysubby@users.noreply.github.com>
2022-11-11 21:59:38 +0530

    Update main.tf

--- shuuu/main.tf
+++ shuuu/main.tf
@@ -1,17 +1,17 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "imagerg234"
+  name     = "imagerg2345"
   location = "East us2"
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "acrproject32676e"
+  name                = "acrproject32676FGFe"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aksproject23263e"
+  name                = "aksproject23263FGFe"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "aksaks3e1"
