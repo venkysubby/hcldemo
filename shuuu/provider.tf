f425971605b8bb2baef50f07600cdd508c994062
venkysubby <91669076+venkysubby@users.noreply.github.com>
2022-11-11 21:05:19 +0530

    Update provider.tf

--- shuuu/provider.tf
+++ shuuu/provider.tf
@@ -7,10 +7,10 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "ptkstorage"
+    storage_account_name = "nokiatram2stroge"
     container_name       = "newcontainer"
     key                  = "prod.terraform.tfstate"
-    access_key = "ne3pDRGWEdoTAWAsLckFYiqIkUthdNbtcxp2WbevpHaroB6JaOKxIpSDtt2SkdMnzhyvtUyAqZVT+AStUBI6+g=="
+    access_key = "bbGnjoQjm6A7W2VdKDapuR336Ry9Sw6qqGfdAfU8f9g8Wv95TPCp7G6hQLW0+RhsSm3dDT8dox/v+AStKcUfVg=="
   }
 }
            
